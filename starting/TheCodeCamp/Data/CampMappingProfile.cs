9a3b80c11251286dbcfb1248f064fd541b9b1a6a
Quinn <quinn4dev@gmail.com>
2019-11-03 23:04:39 -0500

    using query strings

---
+++ starting/TheCodeCamp/Data/CampMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Data
+{
+    public class CampMappingProfile : Profile
+    {
+        public CampMappingProfile()
+        {
+            CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, opt => opt.MapFrom(m => m.Location.VenueName));
+            CreateMap<Talk, TalkModel>()
+                .ForMember(c => c.TalkId, opt => opt.MapFrom(m => m.TalkId));
+            CreateMap<Speaker, SpeakerModel>()
+                .ForMember(c => c.SpeakerId, opt => opt.MapFrom(m => m.SpeakerId));
+        }
+    }
+}
